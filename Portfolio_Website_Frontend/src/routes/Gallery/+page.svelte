8f2bdbfac1e557bf640849a80aeaf14d53cee313
Toby Looijkens <138786228+Toby-Looijkens@users.noreply.github.com>
2025-01-12 22:08:31 +0100

    Enabled CORS

--- Portfolio_Website_Frontend/src/routes/Gallery/+page.svelte
+++ Portfolio_Website_Frontend/src/routes/Gallery/+page.svelte
@@ -1,7 +1,18 @@
-<script></script>
+<script>
+  import { onMount } from "svelte";
+  export let data;
+
+  onMount(async () => {
+    const url = "http://localhost:27777/api/GalleryManager/GetAllGalleryItems";
+    const res = await fetch(url);
+    console.log(res);
+    const product = await res.json();
+    return { props: { data } };
+  });
+</script>
 
 <body>
-  <h1>Zooi</h1>
+  <h1>Gallery {data}</h1>
 </body>
 
 <style>
--- Unit_Tests/UnitTest1.cs
+++ Unit_Tests/UnitTest1.cs
@@ -1,3 +1,7 @@
+using Logic.DTOs;
+using Logic.Entities;
+using Logic.Utils;
+
 namespace Unit_Tests
 {
     public class Tests
@@ -8,9 +12,33 @@ namespace Unit_Tests
         }
 
         [Test]
-        public void Test1()
+        public void DoesMapperMapTagDTOToTag()
         {
-            Assert.Pass();
+            //Arrange
+            Guid guid = Guid.NewGuid();
+            TagDTO dto = new TagDTO(guid, "test");
+            Tag testTag;
+            Tag tag = new Tag(guid, "test");
+            //Act
+            testTag = Mapper.MapTagDTOToTag(dto);
+            //Assert
+            Assert.AreEqual(testTag.ID, tag.ID);
+            Assert.AreEqual(testTag.Name, tag.Name);
+        }
+
+        [Test]
+        public void DoesMapperMapTagDTOToTag_EmptyGUID()
+        {
+            //Arrange
+            Guid guid = Guid.Empty;
+            TagDTO dto = new TagDTO(guid, "test");
+            Tag testTag;
+            Tag tag = new Tag(guid, "test");
+            //Act
+            testTag = Mapper.MapTagDTOToTag(dto);
+            //Assert
+            Assert.AreEqual(testTag.ID, tag.ID);
+            Assert.AreEqual(testTag.Name, tag.Name);
         }
     }
 }
